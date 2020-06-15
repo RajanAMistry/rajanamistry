cc40ba0c4a70b4921bc2418fa2dade2130d35220
rajanamistry <rajanamistry@gmail.com>
2020-06-15 13:14:55 +0530

    fix(resume): fixed links and mobile view

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-function navigateMenu(){
-	
-}
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+$('.nav-link').on('click', function (evt) {
+	$('.nav-link.active').toggleClass('active');
+    $(this).toggleClass('active');
+});
